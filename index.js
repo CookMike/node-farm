324ab5704894dc3a331aae470aaf5ff86b09f35b
Michal Kuchar <kucharmichalnl@gmail.com>
2021-07-29 11:59:15 +0100

    dependencies

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 
 const replaceTemplate = require("./modules/replaceTemplate");
+const { CLIENT_RENEG_LIMIT } = require("tls");
 
+//SERVER
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -18,6 +21,8 @@ const tempProduct = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
